e3837d052c1611071a2d7b239043cff1a96af4a1
Jesse Gallagher <jesse@secondfoundation.org>
2019-03-28 17:24:34 -0400

    Rename packages again

---
+++ xpages-webapp-support/xpages-runtime/src/main/java/org/openntf/xpages/runtime/osgi/MockBundleContext.java
@@ -0,0 +1,186 @@
+/**
+ * Copyright © 2019 Jesse Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openntf.xpages.runtime.osgi;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Dictionary;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.BundleListener;
+import org.osgi.framework.Filter;
+import org.osgi.framework.FrameworkListener;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceFactory;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceObjects;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+
+public class MockBundleContext implements BundleContext {
+	
+	private final MockBundle bundle;
+	
+	public MockBundleContext(MockBundle bundle) {
+		this.bundle = bundle;
+	}
+
+	@Override
+	public Bundle getBundle() {
+		return this.bundle;
+	}
+
+	@Override
+	public void addBundleListener(BundleListener listener) {
+
+	}
+
+	@Override
+	public void addFrameworkListener(FrameworkListener listener) {
+
+	}
+
+	@Override
+	public void addServiceListener(ServiceListener listener) {
+
+	}
+
+	@Override
+	public void addServiceListener(ServiceListener arg0, String arg1) throws InvalidSyntaxException {
+
+	}
+
+	@Override
+	public Filter createFilter(String arg0) throws InvalidSyntaxException {
+		return null;
+	}
+
+	@Override
+	public ServiceReference<?>[] getAllServiceReferences(String arg0, String arg1) throws InvalidSyntaxException {
+		return null;
+	}
+
+	@Override
+	public Bundle getBundle(long arg0) {
+		return null;
+	}
+
+	@Override
+	public Bundle getBundle(String arg0) {
+		return null;
+	}
+
+	@Override
+	public Bundle[] getBundles() {
+		return null;
+	}
+
+	@Override
+	public File getDataFile(String arg0) {
+		return null;
+	}
+
+	@Override
+	public String getProperty(String arg0) {
+		return null;
+	}
+
+	@Override
+	public <S> S getService(ServiceReference<S> arg0) {
+		return null;
+	}
+
+	@Override
+	public <S> ServiceObjects<S> getServiceObjects(ServiceReference<S> arg0) {
+		return null;
+	}
+
+	@Override
+	public ServiceReference<?> getServiceReference(String arg0) {
+		return null;
+	}
+
+	@Override
+	public <S> ServiceReference<S> getServiceReference(Class<S> arg0) {
+		return null;
+	}
+
+	@Override
+	public ServiceReference<?>[] getServiceReferences(String arg0, String arg1) throws InvalidSyntaxException {
+		return null;
+	}
+
+	@Override
+	public <S> Collection<ServiceReference<S>> getServiceReferences(Class<S> arg0, String arg1)
+			throws InvalidSyntaxException {
+		return null;
+	}
+
+	@Override
+	public Bundle installBundle(String arg0) throws BundleException {
+		return null;
+	}
+
+	@Override
+	public Bundle installBundle(String arg0, InputStream arg1) throws BundleException {
+		return null;
+	}
+
+	@Override
+	public ServiceRegistration<?> registerService(String[] arg0, Object arg1, Dictionary<String, ?> arg2) {
+		return null;
+	}
+
+	@Override
+	public ServiceRegistration<?> registerService(String arg0, Object arg1, Dictionary<String, ?> arg2) {
+		return null;
+	}
+
+	@Override
+	public <S> ServiceRegistration<S> registerService(Class<S> arg0, S arg1, Dictionary<String, ?> arg2) {
+		return null;
+	}
+
+	@Override
+	public <S> ServiceRegistration<S> registerService(Class<S> arg0, ServiceFactory<S> arg1,
+			Dictionary<String, ?> arg2) {
+		return null;
+	}
+
+	@Override
+	public void removeBundleListener(BundleListener arg0) {
+
+	}
+
+	@Override
+	public void removeFrameworkListener(FrameworkListener arg0) {
+
+	}
+
+	@Override
+	public void removeServiceListener(ServiceListener arg0) {
+
+	}
+
+	@Override
+	public boolean ungetService(ServiceReference<?> arg0) {
+		return false;
+	}
+
+}
