6345e69cdb06cb9eeeae939b3acc0be6bccf77b5
Jesse Gallagher <jesse@secondfoundation.org>
2019-03-28 17:24:11 -0400

    Rename packages

---
+++ xpages-webapp-support/xpages-runtime/src/main/java/org/openntf/xpagesruntime/wrapper/LibertyServletRequestWrapper.java
@@ -0,0 +1,406 @@
+/**
+ * Copyright © 2019 Jesse Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openntf.xpagesruntime.wrapper;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import com.ibm.designer.runtime.domino.bootstrap.adapter.HttpServletRequestAdapter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Map;
+
+public class LibertyServletRequestWrapper implements HttpServletRequest, HttpServletRequestAdapter {
+    private final HttpServletRequest delegate;
+	
+	private Principal overridePrincipal;
+
+    public LibertyServletRequestWrapper(HttpServletRequest delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+    	if(this.overridePrincipal != null) {
+    		return this.overridePrincipal;
+    	}
+    	Principal d = delegate.getUserPrincipal();
+    	if(d == null) {
+    		return () -> "Anonymous";
+    	}
+    	return d;
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return new LibertyServletContextWrapper(delegate.getServletContext());
+    }
+
+    @Override
+    public String getAuthType() {
+        return delegate.getAuthType();
+    }
+
+    @Override
+    public Cookie[] getCookies() {
+        return delegate.getCookies();
+    }
+
+    @Override
+    public long getDateHeader(String name) {
+        return delegate.getDateHeader(name);
+    }
+
+    @Override
+    public String getHeader(String name) {
+        return delegate.getHeader(name);
+    }
+
+    @Override
+    public Enumeration<String> getHeaders(String name) {
+        return delegate.getHeaders(name);
+    }
+
+    @Override
+    public Enumeration<String> getHeaderNames() {
+        return delegate.getHeaderNames();
+    }
+
+    @Override
+    public int getIntHeader(String name) {
+        return delegate.getIntHeader(name);
+    }
+
+    @Override
+    public String getMethod() {
+        return delegate.getMethod();
+    }
+
+    @Override
+    public String getPathInfo() {
+        return delegate.getPathInfo();
+    }
+
+    @Override
+    public String getPathTranslated() {
+        return delegate.getPathTranslated();
+    }
+
+    @Override
+    public String getContextPath() {
+        return delegate.getContextPath();
+    }
+
+    @Override
+    public String getQueryString() {
+        return delegate.getQueryString();
+    }
+
+    @Override
+    public String getRemoteUser() {
+        return delegate.getRemoteUser();
+    }
+
+    @Override
+    public boolean isUserInRole(String role) {
+        return delegate.isUserInRole(role);
+    }
+
+    @Override
+    public String getRequestedSessionId() {
+        return delegate.getRequestedSessionId();
+    }
+
+    @Override
+    public String getRequestURI() {
+        return delegate.getRequestURI();
+    }
+
+    @Override
+    public StringBuffer getRequestURL() {
+        return delegate.getRequestURL();
+    }
+
+    @Override
+    public String getServletPath() {
+        return delegate.getServletPath();
+    }
+
+    @Override
+    public HttpSession getSession(boolean create) {
+        return delegate.getSession(create);
+    }
+
+    @Override
+    public HttpSession getSession() {
+        return delegate.getSession();
+    }
+
+    @Override
+    public String changeSessionId() {
+        return delegate.changeSessionId();
+    }
+
+    @Override
+    public boolean isRequestedSessionIdValid() {
+        return delegate.isRequestedSessionIdValid();
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromCookie() {
+        return delegate.isRequestedSessionIdFromCookie();
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromURL() {
+        return delegate.isRequestedSessionIdFromURL();
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean isRequestedSessionIdFromUrl() {
+        return delegate.isRequestedSessionIdFromUrl();
+    }
+
+    @Override
+    public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+        return delegate.authenticate(response);
+    }
+
+    @Override
+    public void login(String username, String password) throws ServletException {
+        delegate.login(username, password);
+    }
+
+    @Override
+    public void logout() throws ServletException {
+        delegate.logout();
+    }
+
+    @Override
+    public Collection<Part> getParts() throws IOException, ServletException {
+        return delegate.getParts();
+    }
+
+    @Override
+    public Part getPart(String name) throws IOException, ServletException {
+        return delegate.getPart(name);
+    }
+
+    @Override
+    public <T extends HttpUpgradeHandler> T upgrade(Class<T> handlerClass) throws IOException, ServletException {
+        return delegate.upgrade(handlerClass);
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        return delegate.getAttribute(name);
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+        return delegate.getAttributeNames();
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return delegate.getCharacterEncoding();
+    }
+
+    @Override
+    public void setCharacterEncoding(String env) throws UnsupportedEncodingException {
+        delegate.setCharacterEncoding(env);
+    }
+
+    @Override
+    public int getContentLength() {
+        return delegate.getContentLength();
+    }
+
+    @Override
+    public long getContentLengthLong() {
+        return delegate.getContentLengthLong();
+    }
+
+    @Override
+    public String getContentType() {
+        return delegate.getContentType();
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        return delegate.getInputStream();
+    }
+
+    @Override
+    public String getParameter(String name) {
+        return delegate.getParameter(name);
+    }
+
+    @Override
+    public Enumeration<String> getParameterNames() {
+        return delegate.getParameterNames();
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        return delegate.getParameterValues(name);
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+        return delegate.getParameterMap();
+    }
+
+    @Override
+    public String getProtocol() {
+        return delegate.getProtocol();
+    }
+
+    @Override
+    public String getScheme() {
+        return delegate.getScheme();
+    }
+
+    @Override
+    public String getServerName() {
+        return delegate.getServerName();
+    }
+
+    @Override
+    public int getServerPort() {
+        return delegate.getServerPort();
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return delegate.getReader();
+    }
+
+    @Override
+    public String getRemoteAddr() {
+        return delegate.getRemoteAddr();
+    }
+
+    @Override
+    public String getRemoteHost() {
+        return delegate.getRemoteHost();
+    }
+
+    @Override
+    public void setAttribute(String name, Object o) {
+        delegate.setAttribute(name, o);
+    }
+
+    @Override
+    public void removeAttribute(String name) {
+        delegate.removeAttribute(name);
+    }
+
+    @Override
+    public Locale getLocale() {
+        return delegate.getLocale();
+    }
+
+    @Override
+    public Enumeration<Locale> getLocales() {
+        return delegate.getLocales();
+    }
+
+    @Override
+    public boolean isSecure() {
+        return delegate.isSecure();
+    }
+
+    @Override
+    public RequestDispatcher getRequestDispatcher(String path) {
+        return delegate.getRequestDispatcher(path);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public String getRealPath(String path) {
+        return delegate.getRealPath(path);
+    }
+
+    @Override
+    public int getRemotePort() {
+        return delegate.getRemotePort();
+    }
+
+    @Override
+    public String getLocalName() {
+        return delegate.getLocalName();
+    }
+
+    @Override
+    public String getLocalAddr() {
+        return delegate.getLocalAddr();
+    }
+
+    @Override
+    public int getLocalPort() {
+        return delegate.getLocalPort();
+    }
+
+    @Override
+    public AsyncContext startAsync() throws IllegalStateException {
+        return delegate.startAsync();
+    }
+
+    @Override
+    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
+        return delegate.startAsync(servletRequest, servletResponse);
+    }
+
+    @Override
+    public boolean isAsyncStarted() {
+        return delegate.isAsyncStarted();
+    }
+
+    @Override
+    public boolean isAsyncSupported() {
+        return delegate.isAsyncSupported();
+    }
+
+    @Override
+    public AsyncContext getAsyncContext() {
+        return delegate.getAsyncContext();
+    }
+
+    @Override
+    public DispatcherType getDispatcherType() {
+        return delegate.getDispatcherType();
+    }
+
+	@Override
+	public String getConversationId() {
+		// TODO ???
+		return delegate.getRequestedSessionId();
+	}
+
+	@Override
+	public void setUserPrincipal(Principal var1) {
+//		System.out.println("Setting user principal to " + var1);
+		this.overridePrincipal = var1;
+	}
+}
